31612ac827386b45aeef30c6c94c136b65461c4e
Luis Henrique <noreply@noreply.com>
2021-07-24 13:34:17 -0300

    Update AppVersion.php

--- app/Config/AppVersion.php
+++ app/Config/AppVersion.php
@@ -13,7 +13,7 @@ class AppVersion extends \CodeIgniter\Config\BaseConfig
 	@var string
 	 Sets cache version for jsManager/CSS/IMG files
 	 */
-	public $css = '20210723_v1';
+	public $css = '20210724_v1';
 	
 	/*
 	@var bool
