20a813528dc9143a9a09cac7661ca8cd152c3cee
Luis Henrique <noreply@noreply.com>
2021-09-26 21:35:43 -0300

    Ulpdate Readme

--- base_table.sql
+++ base_table.sql
@@ -233,4 +233,6 @@ INSERT INTO grupos (id, nome, ativo, deletado, data_criacao, usuario_criacao, da
 (1, 'Administrador do Sistema', 1, 0, '2021-09-19 22:07:10', 'asd-1s1s-3bnmvhj', '2021-09-19 22:07:10', 'asd-1s1s-3bnmvhj'),
 (2, 'Regular', 1, 0, '2021-09-19 22:07:19', 'asd-1s1s-3bnmvhj', '2021-09-19 22:07:19', 'asd-1s1s-3bnmvhj');
 
+INSERT INTO parametros (id, nome, codigo, descricao, valor, deletado, data_criacao, usuario_criacao, data_modificacao, usuario_modificacao) VALUES ('0', 'Criar Conta pela tela de login', 'enable_create_user_login', '0 - Desabilitado\r\n1 - Habilitado', '1', '0', '2021-09-26 21:04:50', 'asd-1s1s-3bnmvhj', '2021-09-26 21:09:56', 'asd-1s1s-3bnmvhj');
+
 INSERT INTO usuarios (id, nome, deletado, data_criacao, usuario_criacao, data_modificacao, usuario_modificacao, email, senha, tipo, last_ip, last_connected, hash_esqueci_senha, ultima_troca_senha, dark_mode, status) VALUES ('asd-1s1s-3bnmvhj', 'Luis', 0, '2020-09-18 09:36:16', 'asd-1s1s-3bnmvhj', '2021-09-19 22:30:44', NULL, 'minoruluis@gmail.com', '202cb962ac59075b964b07152d234b70', '1', '192.168.1.175', '2021-09-19 22:30:44', NULL, '2021-08-07 10:59:55', 1, 'ativo');
